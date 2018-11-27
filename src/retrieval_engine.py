28c08015203873cc10376083b41543bd95a4232b
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-11-27 17:47:30 +0100

    Add search collection parser

--- src/retrieval_engine.py
+++ src/retrieval_engine.py
@@ -2,22 +2,23 @@ import re
 import xml.etree.ElementTree as ET
 
 
-def clean_search_query(query):
+def clean_text(query):
     query = re.sub(r'[^a-zA-Z0-9 ]', '', query)
     query = re.sub(r'\s+', ' ', query)
     query = query.strip()
     return query
 
 
-def parse_search_queries(document):
-    queries = {}
+def parse_trec(document):
+    result = {}
     tree = ET.parse(document)
     root = tree.getroot()
     for doc in root:
-        queries[doc[0].text] = clean_search_query(doc[1].text)
-    return queries
+        result[doc[0].text] = clean_text(doc[1].text)
+    return result
 
 
 if __name__ == '__main__':
-    search_queries = parse_search_queries('documents/irg_queries.trec')
+    search_queries = parse_trec('documents/irg_queries.trec')
+    search_collections = parse_trec('documents/irg_collection.trec')
     print("hello")
