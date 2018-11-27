7e71e820d085bdfed7b8c4009d9a1b9f3cf76008
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-11-27 21:54:25 +0100

    Add pytest

--- src/retrieval_engine.py
+++ src/retrieval_engine.py
@@ -21,34 +21,20 @@ def parse_trec(document):
 
 
 def min_distance(query, collection):
-    # TODO: Eliminate stop of collections (at some point)
-    pass
+    search_terms = query.split(' ')
+    document_words = collection.split(' ')
 
-    # search_terms = query.split(' ')
-    # document_words = collection.split(' ')
+    words_info = []
 
-    # words_distance = []
-
-    # word_id = 0
-    # for word in document_words:
-    #     word_id = word_id + 1
-    #     words_distance.
-    #     # print(f'Current word: {current_word}')
-    #     min_distance = 0
-    #     for iterate_word in document_words:
-    #         # print(f'Iterating: {iterate_word}')
-    #         min_distance += 1
-    #         if iterate_word in search_terms:
-    #             min_distance = 0
-    #             print(f'=== {iterate_word} is in search terms')
-    #     words_distance.append({'word': current_word, 'distance': min_distance})
+    word_index = 0
+    for word in document_words:
+        word_index = word_index + 1
+        words_info.append({'index': word_index, 'word': word, 'min_distance': None})
 
 
 if __name__ == '__main__':
     search_queries = parse_trec('documents/irg_queries.trec')
     search_collections = parse_trec('documents/irg_collection_short.trec')
-    print("hello")
 
     word_list = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog", "."]
     filtered_words = [word for word in word_list if word not in stopwords.words('english')]
-    print(filtered_words)
--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -2,7 +2,7 @@ from src.retrieval_engine import min_distance
 
 
 def should_calculate_word_distances():
-    search_queries = {'245': 'z'}
-    search_collections = {'543': 'x y z a b c'}
-    result = min_distance(search_queries, search_collections)
+    search_query = 'z'
+    search_collection = 'x y z a b c'
+    result = min_distance(search_query, search_collection)
     assert result == {'x': 2, 'y': 1, 'a': 1, 'b': 2, 'c': 3}
