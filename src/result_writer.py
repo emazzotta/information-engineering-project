351d929d5c4cc1e2fcdc2d3770bd89bbf285d170
Leandro Kuster <leandro@kusterfamily.com>
2018-11-27 18:19:40 +0100

    Add result writer and result class

--- src/result_writer.py
+++ src/result_writer.py
@@ -1,21 +1,21 @@
 from src.result import Result
 
+
 def output_formatter(results):
     result_list = []
     for result in results:
-        result_line = str(result.query_id)
-        result_line += " " + result.iteration
-        result_line += " " + result.doc_number
-        result_line += " " + str(result.rank)
-        result_line += " " + str(result.score)
-        result_line += " " + result.system
-        result_list.append(result_line)
+        result_formatted = str(result.query_id)
+        result_formatted += " " + result.iteration
+        result_formatted += " " + result.doc_number
+        result_formatted += " " + str(result.rank)
+        result_formatted += " " + str(result.score)
+        result_formatted += " " + result.system
+        result_list.append(result_formatted)
     return result_list
 
+
 if __name__ =='__main__':
-    resultObject1 = Result(10, "Q0", "docNumber1", 1, 2.5)
-    resultObject2 = Result(10, "Q0", "docNumber2", 2, 2.2)
-
-    resultObjects = [resultObject1, resultObject2]
-
-    output_formatter(resultObjects)
+    result1 = Result(10, "Q0", "docNumber1", 1, 2.5)
+    result2 = Result(10, "Q0", "docNumber2", 2, 2.2)
+    test_results = [result1, result2]
+    output_formatter(test_results)
