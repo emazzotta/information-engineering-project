be920644a53baa942357c69cab07b76649ae688e
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-12-02 14:51:55 +0100

    Perform refactoring

--- src/retrieval_engine.py
+++ src/retrieval_engine.py
@@ -24,16 +24,18 @@ def parse_trec(document):
     return result
 
 
-if __name__ == '__main__':
-    # Load xml data
-    search_queries = parse_trec('documents/irg_queries.trec')
-    search_collections = parse_trec('documents/irg_collection_short.trec')
-
-    # Remove stop words in collections
+def eliminate_stopwords(search_collections):
     nltk.download('stopwords')
     for key, value in search_collections.items():
         filtered = ' '.join([word for word in value.split(' ') if word not in stopwords.words('english')])
         search_collections[key] = filtered
+    return search_collections
+
+
+def retrieve_results():
+    search_queries = parse_trec('documents/irg_queries.trec')
+    search_collections = parse_trec('documents/irg_collection_short.trec')
+    search_collections = eliminate_stopwords(search_collections)
 
     # Remove worst 10%-ile => 90%-ile most farthest
     for search_query_id, search_query_text in search_queries.items():
@@ -42,4 +44,8 @@ if __name__ == '__main__':
             worst_10_percentile = int(percentile(sorted(distances.values()), percent=0.9))
             new_words = [word for word, distance in distances.items() if distance <= worst_10_percentile]
 
-    print("hello")
+    # TF-IDF
+
+
+if __name__ == '__main__':
+    retrieve_results()
