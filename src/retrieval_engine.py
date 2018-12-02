53405edd6f3be16f361444f1f3b45630c20180a6
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2018-12-02 17:07:51 +0100

    Add somewhat working state

--- src/retrieval_engine.py
+++ src/retrieval_engine.py
@@ -38,37 +38,37 @@ def retrieve_results():
     search_collections = parse_trec('documents/irg_collection_short.trec')
     search_collections = eliminate_stopwords(search_collections)
 
-    # id: search query id, value: list of search collection texts
-    query_collection_store = remove_n_percentile_most_farthest_words(search_collections, search_queries, n=0.9)
+    search_queries = {1: 'lol test', 2: 'test'}
+    search_collections = {200: 'haha the sky is so lol blue', 300: 'lol this is a test', 400: 'hmm test'}
 
     # TF-IDF
-    search_query_tf_idfs = {}
+    queries_scores = {}
     for search_query_id, search_query_text in search_queries.items():
-        search_query_text_list = search_query_text.split(' ')
-        search_texts_collection = TextCollection(query_collection_store[search_query_id])
-        tf_idf_scores = {}
-        for search_query_term in search_query_text_list:
-            term = TextCollection(search_query_term)
-            tf_idf_scores[search_query_term] = search_texts_collection.tf_idf(term)
-        search_query_tf_idfs[search_query_id] = tf_idf_scores
+        terms = search_query_text.split(' ')
+        documents = remove_n_percentile_most_farthest_words(search_collections, search_query_text, n=0.9)
+        document_scores = {}
+        search_texts_collection = TextCollection(documents.values())
+        for document_id, document_text in documents.items():
+            for term in terms:
+                current_score = document_scores.get(document_id, 0.0)
+                document_scores[document_id] = current_score + search_texts_collection.tf_idf(term, document_text)
+        queries_scores[search_query_id] = document_scores
+
+    print("hello")
 
 
-def remove_n_percentile_most_farthest_words(search_collections, search_queries, n):
-    query_collection_store = {}
-    for search_query_id, search_query_text in search_queries.items():
-        specific_collection = []
-        for search_collection_id, search_collection_text in search_collections.items():
-            distances = min_distance(search_query_text, search_collection_text)
-            worst_percentile = int(percentile(sorted(distances.values()), percent=n))
-            new_words = ' '.join([word for word in search_collection_text.split(' ') if distances[word] <= worst_percentile])
-            if len(new_words) != len(search_collection_text):
-                print('Change detected!')
-                print(f'Search query: {search_query_text}')
-                print(f'Search collection: {search_collection_text}')
-            specific_collection.append(new_words)
-        query_collection_store[search_query_id] = specific_collection
-
-    return query_collection_store
+def remove_n_percentile_most_farthest_words(search_collections, query_text, n):
+    result = {}
+    for search_collection_id, search_collection_text in search_collections.items():
+        distances = min_distance(query_text, search_collection_text)
+        worst_percentile = int(percentile(sorted(distances.values()), percent=n))
+        new_words = ' '.join([word for word in search_collection_text.split(' ') if distances[word] <= worst_percentile])
+        if len(new_words) != len(search_collection_text):
+            print('Change detected!')
+            print(f'Search query: {query_text}')
+            print(f'Search collection: {search_collection_text}')
+        result.update({search_collection_id: new_words})
+    return result
 
 
 if __name__ == '__main__':
